0ff2cb80015710bfdf4bc1bb74173e2cb0e51d94
Micheletti <73291937+joaoMicheletti@users.noreply.github.com>
2025-01-12 01:43:12 -0300

    update
    atualizadas as funcionalidades de pagamento e regitro de plano

--- project-back-end/src/providers/salao.providers/login.service.ts
+++ project-back-end/src/providers/salao.providers/login.service.ts
@@ -15,7 +15,8 @@ export class Login {
         if (cCpf.length === 0) { // If zero not have a salon with this "cpf_salao"
             // Search in table employees
             const funcionario = await connection('funcionarios').where('cpf_funcionario', cpf_salao).select('*');
-            console.log(funcionario, funcionario.length === 0);
+            
+            
             
             if (funcionario.length === 0) { // Not found employee with this cpf
                 console.log('caiu aqui');
@@ -34,6 +35,7 @@ export class Login {
                     const status = await connection('salao').where('cpf_salao', cpfSalao).select('assinatura_status');
                     
                     if (status[0].assinatura_status === 'on') {
+                        console.log('here >>>>>> ', status[0].assinatura_status)
                         // Employee access granted, return object with cpf_funcionario
                         const Data = { cpf_funcionario: funcionario[0].cpf_funcionario };
                         return Data;
@@ -154,15 +156,16 @@ export class Login {
     };
     async StatusAssinatura(data : LoginDto): Promise<object>{
         const {cpf_salao} = data;
-       //como essa função vai ser chamada constantemente ela tratara de verificar a data de termino de assinatur e etualizar o status de assinatura;
-        //data de vencimento do plano do salão.
-        let dataVencimento = await connection('salao').where('cpf_salao', cpf_salao).select('data_vencimento_plano');
-        //verificar se os dis free ainda estão liberados.
-        if(dataVencimento[0].data_vencimento_plano === null){
-            // si for nul não, ainda nao contratou nenum plano, então verificaremos os dis free;
-            var dataAtual = new Date();
-            var cad = await connection('salao').where('cpf_salao', cpf_salao).select('data_cadastro');
-            var partes = cad[0].data_cadastro.split('/');
+        
+        let confimartion = await connection('salao').where('cpf_salao', cpf_salao).select('data_vencimento_plano');
+        if(confimartion.length === 0) { // se for 0 o valor dentro da variavel cpf salao é o cpf do funcionario
+            var t = await connection('funcionarios').where('cpf_funcionario', cpf_salao).select("*");
+            var dataV = await connection('salao').where('cpf_salao', t[0].cpf_salao).select('data_vencimento_plano');
+            if(dataV[0].data_vencimento_plano === null){
+                // si for nul, ainda nao contratou nenum plano, então verificaremos os dis free;
+                var dataAtual = new Date();
+                var cad = await connection('salao').where('cpf_salao',  t[0].cpf_salao).select('data_cadastro');
+                var partes = cad[0].data_cadastro.split('/');
                 var dia = parseInt(partes[0], 10);
                 var mes = parseInt(partes[1], 10) - 1; // Os meses em JavaScript são baseados em zero
                 var ano = parseInt(partes[2], 10);
@@ -171,8 +174,58 @@ export class Login {
                 // Adicionar 7 dias ao objeto de data
                 Data.setDate(Data.getDate() + 7);
                 //salvando na variavel o status dos dias free, true para acesso livre false para acesso livre excedido;
-                var dias_free = dataAtual < Data;
-                return {res: 'dias_free'}
+                if(Data < dataAtual){
+                    return {res: 'Seus dias de acesso livre a plataforma acabaram, contrate um plano.'}
+    
+                } else {
+                    return {res: 'ok'};
+                }
+            };
+            // Quebrar a string da data vencimento em dia, mês e ano
+            var partes = dataV[0].data_vencimento_plano.split('/');
+            var Dia = parseInt(partes[0], 10);
+            var Mes = parseInt(partes[1], 10) - 1 ; // Os meses em JavaScript são baseados em zero
+            var Ano = parseInt(partes[2], 10);
+            const dataVencimentobject = new Date(Ano, Mes, Dia);
+            // data Atual;
+            const DataAtual = new Date();
+            var dia = DataAtual.getDate();
+            var mes = DataAtual.getMonth();
+            var ano = DataAtual.getFullYear();
+            const dataAtualObject = new Date(ano, mes, dia);
+            //foi criado dois objetos de data para facilitar na conparação das datas 
+            if(dataAtualObject > dataVencimentobject){ // verificação de periodo final do plano
+                await connection('salao').where('cpf_salao',  t[0].cpf_salao).update('assinatura_status', 'off');
+                return {res: 'null'};
+            };
+            return {res: 'ok'}
+        };
+        
+       //como essa função vai ser chamada constantemente ela tratara de verificar a data de termino de assinatur e etualizar o status de assinatura;
+        //data de vencimento do plano do salão.
+        let dataVencimento = await connection('salao').where('cpf_salao', cpf_salao).select('data_vencimento_plano');
+        //verificar se os dis free ainda estão liberados.
+        
+        if(dataVencimento[0].data_vencimento_plano === null){
+            // si for nul, ainda nao contratou nenum plano, então verificaremos os dis free;
+            var dataAtual = new Date();
+            var cad = await connection('salao').where('cpf_salao', cpf_salao).select('data_cadastro');
+            var partes = cad[0].data_cadastro.split('/');
+            var dia = parseInt(partes[0], 10);
+            var mes = parseInt(partes[1], 10) - 1; // Os meses em JavaScript são baseados em zero
+            var ano = parseInt(partes[2], 10);
+            // Criar um objeto de data com os valores obtidos
+            var Data = new Date(ano, mes, dia);
+            // Adicionar 7 dias ao objeto de data
+            Data.setDate(Data.getDate() + 7);
+            //salvando na variavel o status dos dias free, true para acesso livre false para acesso livre excedido;
+            if(Data < dataAtual){
+                return {res: 'Seus dias de acesso livre a plataforma acabaram, contrate um plano.'}
+
+            } else {
+                return {res: 'ok'};
+            }
+            
         }
         // Quebrar a string da data vencimento em dia, mês e ano
         var partes = dataVencimento[0].data_vencimento_plano.split('/');
